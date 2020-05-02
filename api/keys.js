5165e189741c54ed23e67e7aa247bdaf810d2dc8
Ron <moreroron@gmail.com>
2020-05-02 13:37:53 +0300

    fix mb

--- api/keys.js
+++ api/keys.js
@@ -13,4 +13,7 @@ module.exports = {
     access_token_secret: "",
     bearerToken: "",
   },
+  mapbox: {
+    token: "pk.eyJ1IjoibW9yZXJvcm9uIiwiYSI6ImNrMjY1YjlsdTJwY3EzbnF0YXhiN2l4ODIifQ.wpMnH7OKc8rrwdKrKu4jog",
+  },
 };
