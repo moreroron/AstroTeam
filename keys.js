00e068433559aa4002857d4ffa1a5ae29ddccfec
Ron <moreroron@gmail.com>
2020-08-16 11:13:05 +0300

    fix

---
+++ keys.js
@@ -0,0 +1,22 @@
+module.exports = {
+  google: {
+    clientID: "",
+    clientSecret: "",
+  },
+  session: {
+    cookieKey: "randomPassword",
+  },
+  twitter: {
+    consumer_key: "",
+    consumer_secret: "",
+    access_token_key: "",
+    access_token_secret: "",
+    bearerToken: "",
+  },
+  mapbox: {
+    token: "",
+  },
+  mongodb: {
+    connection_string: ""
+  }
+};
